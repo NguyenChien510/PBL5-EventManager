6d0c766b90ae3add1aa49f4d60a0c9cff5da3e17
Nguyen Le Minh Chien <minhchienvadori@gmail.com>
2026-05-31 17:31:32 +0700

    chore(dev): enable hot reloading for AI server

--- AI/main.py
+++ AI/main.py
@@ -1334,4 +1334,4 @@ async def sync_database():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
